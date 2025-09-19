09f2d3d2b8c312750a47f13b6ee7e95bc599482e
Ryan Fuchs <ryfu@gmx.ch>
2025-09-20 01:19:29 +0200

    Enhanced the login card

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -19,6 +19,17 @@ const LoginPage = ({ onLogin }) => {
 
   return (
     <div className="login-container">
+      <div className="floating-orb"></div>
+      <div className="floating-orb"></div>
+      <div className="floating-orb"></div>
+      <div className="particle particle-1"></div>
+      <div className="particle particle-2"></div>
+      <div className="particle particle-3"></div>
+      <div className="particle particle-4"></div>
+      <div className="particle particle-5"></div>
+      <div className="particle particle-6"></div>
+      <div className="particle particle-7"></div>
+      <div className="particle particle-8"></div>
       <div className="login-card">
         <div className="login-header">
           <h1 className="login-title">Finance Dashboard</h1>
